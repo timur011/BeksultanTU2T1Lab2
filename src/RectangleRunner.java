369d46989ccec9eda1352ba89b8efe5c75047dde
Timur Beksultan <timurb2@nycstudents.net>
2022-09-30 09:28:29 -0400

    added some more methods

---
+++ src/RectangleRunner.java
@@ -0,0 +1,15 @@
+public class RectangleRunner {
+    public static void main(String []args)
+    {
+        Rectangle rect1 = new Rectangle(10, 20);
+        int area = rect1.calculateArea();
+        double volume = rect1.calculateBoxVolume():
+
+
+
+
+    }
+
+
+
+}
