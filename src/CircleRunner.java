0ebd20a13870b8c6fb362b92aa2aa732ba917523
Timur Beksultan <timurb2@nycstudents.net>
2022-09-30 08:25:02 -0400

    finished warm up

---
+++ src/CircleRunner.java
@@ -0,0 +1,20 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        //A. Create a circle with radius 5.0
+
+        Circle circ1 = new Circle(5.0);
+
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circ1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+
+        //I predict that the area of the circle is going to get printed
+
+        // D. run the code and confirm your prediction! Were you correct?
+
+        // I was correct!
+    }
+}
